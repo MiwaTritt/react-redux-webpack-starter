37893df7279828de219bb8ea87f0a62b4ffbbe8b
MiwaTritt <tmt7rc@virginia.edu>
2018-02-03 17:52:21 -0500

    Adding Travis CI + configuration changes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# React, Redux, Webpack Starter Project
+# crypto-tracker
+[![Build Status](https://travis-ci.org/MiwaTritt/crypto-tracker.svg?branch=master)](https://travis-ci.org/MiwaTritt/crypto-tracker)
+[![dependencies](https://david-dm.org/MiwaTritt/crypto-tracker.svg)](https://david-dm.org/MiwaTritt/crypto-tracker)
+[![devDependency Status](https://david-dm.org/MiwaTritt/crypto-tracker/dev-status.svg)](https://david-dm.org/MiwaTritt/crypto-tracker#info=devDependencies)
--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,18 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 //config
-const INDEX_HTML = "./src/index.html";
-const INDEX_JS = "./src/index.js";
+const INDEX_HTML = path.resolve(__dirname, "src/index.html");
+const INDEX_JS = path.resolve(__dirname, "src/index.js");
 
 module.exports = {
   entry: INDEX_JS,
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js"
+    filename: "bundle.js"
   },
   resolve: {
-    modules: [path.resolve("./src"), path.resolve("./node_modules")]
+    extensions: ["*", ".js", ".jsx"],
+    modules: [path.resolve(__dirname, "src/"), "node_modules/"]
   },
   devServer: {
     inline: true,
@@ -25,9 +26,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
-      { test:/\.(s*)css$/, use:['style-loader','css-loader', 'sass-loader'] }
+      { test: /\.(js|jsx)$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.(s*)css$/, use: ["style-loader", "css-loader", "sass-loader"] }
     ]
   },
   plugins: [
